7a02b006554307bb13fafa72536590e92c8f3f19
cmallen2106 <cmallen2106@gmail.com>
2021-07-13 14:08:02 -0600

    Create double objects

--- lab2Ex2/src/DistCalc.java
+++ lab2Ex2/src/DistCalc.java
@@ -8,6 +8,21 @@ import java.text.DecimalFormat;
 public class DistCalc {
 	public static void main(String[]args) {
 		
+		Scanner scan = new Scanner(System.in); 
+		
+		System.out.println("What is X1?"); 
+		double x1 = scan.nextDouble();
+		
+		System.out.println("What is y1?"); 
+		double y1 = scan.nextDouble();
+		
+		System.out.println("What is X2?"); 
+		double x2 = scan.nextDouble();
+		
+		System.out.println("What is y2?"); 
+		double y2 = scan.nextDouble();
+		
+		Math.sqrt((x2-x1) + (y2-y1));
 		
 		// Round the output to three decimal places
 	      DecimalFormat fmt = new DecimalFormat ("0.###");
