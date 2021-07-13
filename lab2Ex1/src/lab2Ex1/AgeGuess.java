dd7371c515dce32f77ca11e80556788f46292ccf
cmallen2106 <cmallen2106@gmail.com>
2021-07-12 21:54:49 -0600

    Added comments

--- lab2Ex1/src/lab2Ex1/AgeGuess.java
+++ lab2Ex1/src/lab2Ex1/AgeGuess.java
@@ -11,16 +11,19 @@ public class AgeGuess {
 		
 		Random randNum = new Random();
 		
+		//Declare a new int variable age &
+		//Initialize age to a random integer between 0 and 100 (inclusive)
 		int age = randNum.nextInt(100); 
 		
 		try (Scanner scan = new Scanner (System.in)) {
 			int ageGuess; 
 			
-			System.out.println("Guess the age:"); //Asking user to gues age
+			//Asks the user for a guess, save the guess into the ageGuess variable
+			System.out.println("Guess the age:"); 
 			ageGuess = scan.nextInt(); 
 			
 			
-			//Reading out name and age to user
+			//Display the user guess and the correct answer
 			System.out.println("The age you guessed is " + ageGuess + ", and the correct age is " + age + "!");
 		}
 		
