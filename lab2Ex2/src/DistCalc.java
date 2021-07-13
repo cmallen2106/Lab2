9e550f4cf6f7b1955539ed1e561e2280f491e1ae
cmallen2106 <cmallen2106@gmail.com>
2021-07-13 14:24:56 -0600

    Created system.out.println message

--- lab2Ex2/src/DistCalc.java
+++ lab2Ex2/src/DistCalc.java
@@ -8,24 +8,26 @@ import java.text.DecimalFormat;
 public class DistCalc {
 	public static void main(String[]args) {
 		
-		Scanner scan = new Scanner(System.in); 
-		
-		System.out.println("What is X1?"); 
-		double x1 = scan.nextDouble();
-		
-		System.out.println("What is y1?"); 
-		double y1 = scan.nextDouble();
-		
-		System.out.println("What is X2?"); 
-		double x2 = scan.nextDouble();
-		
-		System.out.println("What is y2?"); 
-		double y2 = scan.nextDouble();
-		
-		Math.sqrt((x2-x1) + (y2-y1));
-		
-		// Round the output to three decimal places
-	      DecimalFormat fmt = new DecimalFormat ("0.###");
+		try (Scanner scan = new Scanner(System.in)) {
+			System.out.println("What is X1?"); 
+			double x1 = scan.nextDouble();
+			
+			System.out.println("What is y1?"); 
+			double y1 = scan.nextDouble();
+			
+			System.out.println("What is X2?"); 
+			double x2 = scan.nextDouble();
+			
+			System.out.println("What is y2?"); 
+			double y2 = scan.nextDouble();
+			
+			double sqrt = Math.sqrt((x2-x1) + (y2-y1));
+			
+			// Round the output to three decimal places
+			  DecimalFormat fmt = new DecimalFormat ("0.###");
+			  
+			  System.out.println("The distance between ("+x1+","+y1+"),"+"("+x2+","+y2+") " + "is " + fmt.format(sqrt));
+		}
 
 	}
 }
