243a3e9ab4d62d18e5437f4e7e647412dcea43a5
cmallen2106 <cmallen2106@gmail.com>
2021-07-13 16:26:37 -0600

    Copy over code from Ex1

--- lab2Ex4/src/lab2Ex4/AgeGuess.java
+++ lab2Ex4/src/lab2Ex4/AgeGuess.java
@@ -1,5 +1,31 @@
+//Lab 2 - Exercise 4
+//Christina Sosa
+//Programming Fundamentals, Summer 2021
 package lab2Ex4;
 
-public class AgeGuess {
+import java.util.Scanner;
+import java.util.Random;
 
+public class AgeGuess {
+	public static void main(String[]args) {
+		
+		Random randNum = new Random();
+		
+		//Declare a new int variable age &
+		//Initialize age to a random integer between 0 and 100 (inclusive)
+		int age = randNum.nextInt(100); 
+		
+		try (Scanner scan = new Scanner (System.in)) {
+			int ageGuess; 
+			
+			//Asks the user for a guess, save the guess into the ageGuess variable
+			System.out.println("Guess the age:"); 
+			ageGuess = scan.nextInt(); 
+			
+			
+			//Display the user guess and the correct answer
+			System.out.println("The age you guessed is " + ageGuess + ", and the correct age is " + age + "!");
+		}
+		
+	}
 }
