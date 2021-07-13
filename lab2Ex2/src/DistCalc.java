28773a36d7111ddbe5fe957abdb46a12327da272
cmallen2106 <cmallen2106@gmail.com>
2021-07-12 22:35:46 -0600

    Added decimalFormat

--- lab2Ex2/src/DistCalc.java
+++ lab2Ex2/src/DistCalc.java
@@ -1,6 +1,16 @@
+//Lab 2 - Exercise 2
+//Christina Sosa
+//Programming Fundamentals, Summer 2021
+
+import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class DistCalc {
 	public static void main(String[]args) {
 		
+		
+		// Round the output to three decimal places
+	      DecimalFormat fmt = new DecimalFormat ("0.###");
+
 	}
 }
