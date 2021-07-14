865554da8c38090ba72cfc5e166f5b4ee3e1382a
cmallen2106 <cmallen2106@gmail.com>
2021-07-14 09:33:25 -0600

    create if else statements

--- lab2Ex5/src/lab2Ex5/FloatEqu.java
+++ lab2Ex5/src/lab2Ex5/FloatEqu.java
@@ -3,6 +3,19 @@ package lab2Ex5;
 public class FloatEqu {
 
 	public static void main(String[] args) {
+		double a = ((1.0/10)*(1.0/10)); 
+		double b = (1.0/100); 
+		
+		System.out.println(a); 
+		System.out.println(b); 
+		
+		if (a==b) {
+			System.out.println("EQUAL"); 
+		}
+		
+		else {
+			System.out.println("NOT EQUAL"); 
+		}
 
 	}
 
